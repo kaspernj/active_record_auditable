6536cb6c587936591bb027a316aa1a2504565582
kaspernj <k@spernj.org>
2024-03-15 08:49:05 +0000

    Added various helpers for adding audits manually

--- app/models/active_record_auditable/audit.rb
+++ app/models/active_record_auditable/audit.rb
@@ -10,4 +10,16 @@ class ActiveRecordAuditable::Audit < ApplicationRecord
   scope :where_action, ->(action) { joins(:audit_action).where(audit_actions: {action:}) }
 
   delegate :action, to: :audit_action
+
+  before_validation :set_audit_auditable_type
+
+  def action=(action_name)
+    self.audit_action = ActiveRecordAuditable::AuditAction.find_or_create_by!(action: action_name)
+  end
+
+private
+
+  def set_audit_auditable_type
+    self.audit_auditable_type = ActiveRecordAuditable::AuditAuditableType.find_or_create_by!(name: auditable_type) if !audit_auditable_type && auditable_type?
+  end
 end
