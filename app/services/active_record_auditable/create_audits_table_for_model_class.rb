5b11ad4c2194051fcf350214794cca5a588db188
kaspernj <k@spernj.org>
2024-09-01 19:36:04 +0000

    More dedicated table fixes

--- app/services/active_record_auditable/create_audits_table_for_model_class.rb
+++ app/services/active_record_auditable/create_audits_table_for_model_class.rb
@@ -9,14 +9,7 @@ class ActiveRecordAuditable::CreateAuditsTableForModelClass < ActiveRecordAudita
     create_args = create_table_args || {}
 
     ActiveRecord::Migration.new.create_table table_name, **create_args do |t|
-      t.references :auditable, polymorphic: true, type: id_type
-
-      if polymorphic_user_relation
-        t.references :user, polymorphic: true, type: id_type
-      else
-        t.references :user, type: id_type
-      end
-
+      t.references model_class.model_name.param_key.to_sym, null: false, type: id_type
       t.json :audited_changes
       t.references :audit_action, foreign_key: true, null: false, type: id_type
       t.references :audit_auditable_type, foreign_key: true, null: false, type: id_type
