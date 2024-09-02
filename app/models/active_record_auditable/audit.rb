34e8e27143b04fdaf13ee598dbfb4660280f8d66
kaspernj <k@spernj.org>
2024-09-02 08:04:27 +0000

    More refactoring to dedicated tables

--- app/models/active_record_auditable/audit.rb
+++ app/models/active_record_auditable/audit.rb
@@ -1,15 +1,16 @@
-class ActiveRecordAuditable::Audit < ApplicationRecord
+class ActiveRecordAuditable::Audit < ActiveRecordAuditable::BaseAudit
   self.table_name = "audits"
 
-  belongs_to :audit_action, class_name: "ActiveRecordAuditable::AuditAction"
+  belongs_to :audit_auditable_type, class_name: "ActiveRecordAuditable::AuditAuditableType"
+  belongs_to :auditable, optional: true, polymorphic: true
 
-  # serialize :audited_changes, coder: JSON
+  scope :where_type, ->(type) { joins(:audit_auditable_type).where(audit_auditable_types: {name: type}) }
 
-  scope :where_action, ->(action) { joins(:audit_action).where(audit_actions: {action:}) }
+  before_validation :set_audit_auditable_type
 
-  delegate :action, to: :audit_action
+private
 
-  def action=(action_name)
-    self.audit_action = ActiveRecordAuditable::AuditAction.find_or_create_by!(action: action_name)
+  def set_audit_auditable_type
+    self.audit_auditable_type = ActiveRecordAuditable::AuditAuditableType.find_or_create_by!(name: auditable_type) if !audit_auditable_type && auditable_type?
   end
 end
---
+++ app/models/concerns/active_record_auditable/acts_as_json.rb
@@ -0,0 +1,49 @@
+module ActiveRecordAuditable::ActsAsJson
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_json(attribute_name) # rubocop:disable Metrics/PerceivedComplexity
+      return if ActiveRecordAuditable::Audit.connection.class.name.include?("SQLite")
+
+      serialize attribute_name, coder: JSON
+
+      validate do
+        value = __send__(attribute_name)
+
+        if value.is_a?(String)
+          begin
+            JSON.parse(value)
+          rescue JSON::ParserError
+            errors.add(attribute_name, :invalid)
+          end
+        end
+      end
+
+      define_method(attribute_name) do
+        value = super()
+
+        if value.is_a?(String) && value.present?
+          JSON.parse(value)
+        else
+          value
+        end
+      rescue JSON::ParserError
+        super()
+      end
+
+      define_method(:"#{attribute_name}=") do |new_value|
+        if new_value.is_a?(Hash)
+          super(JSON.generate(new_value))
+        elsif new_value.is_a?(String) && new_value.blank?
+          super(nil)
+        else
+          super(new_value)
+        end
+      rescue JSON::ParserError
+        super(new_value)
+      end
+    end
+  end
+end
